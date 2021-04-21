b9cbbf8733f3435bbfcb6e37a0f8c67930fa3140
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-04-21 13:12:29 +0000

    ejercicio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  pronoun.forEach(elementP, index => {
+    adj.forEach(elementA, index => {
+      noun.forEach(elementN, index => {});
+    });
+  });
 };
